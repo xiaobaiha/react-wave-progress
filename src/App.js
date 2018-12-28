f0324f9f6ea6e25f601f4fbd384ac88252ee7466
xiaobaiha <xiaobaihaha0001@163.com>
2018-12-28 20:56:11 +0800

    feat: sin act

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.canvas = React.createRef();
+    this.offset = 0;
   }
   drawCircle = (x, y, r) => {
     this.ctx.fillStyle = 'rgba(46,129,206,1)';
@@ -14,26 +15,40 @@ class App extends Component {
     this.ctx.fill();
   }
   drawWave = () => {
-    const offset = (Date.now() % 1000) / 10 - 50;
+    this.offset += 1;
+    if (this.offset === 100) {
+      this.offset = 0;
+    }
+
+    console.log(this.offset);
+    const r = 50;
+    const offsetHeight = 15;
     this.ctx.beginPath();
     this.ctx.strokeStyle = 'blue';
-    this.ctx.moveTo(0 + offset, 50);
-    this.ctx.quadraticCurveTo(25 + offset, 25, 50 + offset, 50);
-    this.ctx.quadraticCurveTo(75 + offset, 75, 100 + offset, 50);
+    this.ctx.moveTo(-2 * r + this.offset, r);
+    this.ctx.quadraticCurveTo(-3 * r / 2 + this.offset, r - offsetHeight, -r + this.offset, r);
+    this.ctx.quadraticCurveTo(-r / 2 + this.offset, r + offsetHeight, 0 + this.offset, r);
+    this.ctx.quadraticCurveTo(r / 2 + this.offset, r - offsetHeight, r + this.offset, r);
+    this.ctx.quadraticCurveTo(3 * r / 2 + this.offset, r + offsetHeight, 2 * r + this.offset, r);
     this.ctx.stroke();
+  }
+  draw = () => {
+    this.ctx.clearRect(0, 0, 100, 100);
+    this.drawCircle();
+    this.drawWave();
+    requestAnimationFrame(this.draw);
   }
   componentDidMount() {
     const canvas = this.canvas.current;
     if (canvas.getContext) {
       this.ctx = canvas.getContext('2d');
-      this.drawCircle();
-      window.requestAnimationFrame(this.drawWave);
+      requestAnimationFrame(this.draw);
     }
   }
   render() {
     return (
       <div className="App">
-        <canvas ref={this.canvas}></canvas>
+        <canvas ref={this.canvas} width="100" height="100"></canvas>
       </div>
     );
   }
