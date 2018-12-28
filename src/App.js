1a67559a45f1b2de5d79a7d059157e8aadadb729
xiaobaiha <xiaobaihaha0001@163.com>
2018-12-28 17:19:06 +0800

    feat: canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+  }
+  drawCircle = (x, y, r) => {
+    this.ctx.fillStyle = 'rgba(46,129,206,1)';
+    this.ctx.beginPath();
+    this.ctx.arc(50, 50, 50, 0, 2 * Math.PI, true);
+    this.ctx.closePath();
+    this.ctx.fill();
+  }
+  drawWave = () => {
+    const offset = (Date.now() % 1000) / 10 - 50;
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = 'blue';
+    this.ctx.moveTo(0 + offset, 50);
+    this.ctx.quadraticCurveTo(25 + offset, 25, 50 + offset, 50);
+    this.ctx.quadraticCurveTo(75 + offset, 75, 100 + offset, 50);
+    this.ctx.stroke();
+  }
+  componentDidMount() {
+    const canvas = this.canvas.current;
+    if (canvas.getContext) {
+      this.ctx = canvas.getContext('2d');
+      this.drawCircle();
+      window.requestAnimationFrame(this.drawWave);
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <canvas ref={this.canvas}></canvas>
       </div>
     );
   }
