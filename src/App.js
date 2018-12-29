9566bccbd602b77113d76dbc608bf21a83ab65f2
xiaobaiha <xiaobaihaha0001@163.com>
2018-12-29 11:50:40 +0800

    feat: basic function

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import WaveProgress from './WaveProgress';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.canvas = React.createRef();
-    this.offset = 0;
-  }
-  drawCircle = (x, y, r) => {
-    this.ctx.fillStyle = 'rgba(46,129,206,1)';
-    this.ctx.beginPath();
-    this.ctx.arc(50, 50, 50, 0, 2 * Math.PI, true);
-    this.ctx.closePath();
-    this.ctx.fill();
-  }
-  drawWave = () => {
-    this.offset += 1;
-    if (this.offset === 100) {
-      this.offset = 0;
-    }
-
-    console.log(this.offset);
-    const r = 50;
-    const offsetHeight = 15;
-    this.ctx.beginPath();
-    this.ctx.strokeStyle = 'blue';
-    this.ctx.moveTo(-2 * r + this.offset, r);
-    this.ctx.quadraticCurveTo(-3 * r / 2 + this.offset, r - offsetHeight, -r + this.offset, r);
-    this.ctx.quadraticCurveTo(-r / 2 + this.offset, r + offsetHeight, 0 + this.offset, r);
-    this.ctx.quadraticCurveTo(r / 2 + this.offset, r - offsetHeight, r + this.offset, r);
-    this.ctx.quadraticCurveTo(3 * r / 2 + this.offset, r + offsetHeight, 2 * r + this.offset, r);
-    this.ctx.stroke();
-  }
-  draw = () => {
-    this.ctx.clearRect(0, 0, 100, 100);
-    this.drawCircle();
-    this.drawWave();
-    requestAnimationFrame(this.draw);
-  }
-  componentDidMount() {
-    const canvas = this.canvas.current;
-    if (canvas.getContext) {
-      this.ctx = canvas.getContext('2d');
-      requestAnimationFrame(this.draw);
-    }
-  }
   render() {
     return (
       <div className="App">
-        <canvas ref={this.canvas} width="100" height="100"></canvas>
+        <WaveProgress r={50} />
       </div>
     );
   }
---
+++ src/WaveProgress.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+class WaveProgress extends Component {
+    constructor(props) {
+        super(props);
+        if (props.r && !isNaN(+props.r)) {
+            this.r = +props.r;
+        } else {
+            console.error("Invalid radius(props.r):", props.r);
+            this.r = 50;
+        }
+        this.canvas = React.createRef();
+        this.borderCanvas = React.createRef();
+        this.offset = 0;
+    }
+    clipCircle = (r) => {
+        this.ctx.beginPath();
+        this.ctx.arc(this.r, this.r, r, 0, 2 * Math.PI, true);
+        this.ctx.closePath();
+        this.ctx.clip();
+    }
+    drawCircle = (r) => {
+        this.border.fillStyle = 'white';
+        const lineWidth = 2;
+        this.border.lineWidth = lineWidth;
+        this.border.beginPath();
+        this.border.arc(this.r, this.r, this.r - lineWidth / 2, 0, 2 * Math.PI, true);
+        this.border.closePath();
+        this.border.stroke();
+    }
+    drawWave = () => {
+        this.offset += 1;
+        if (this.offset === 2 * this.r) {
+            this.offset = 0;
+        }
+        console.log(this.offset)
+        const offsetHeight = this.r / 5;
+        this.ctx.beginPath();
+        this.ctx.fillStyle = '#B0E2FF';
+        this.ctx.moveTo(-2 * this.r + this.offset, this.r);
+        this.ctx.quadraticCurveTo(-3 * this.r / 2 + this.offset, this.r - offsetHeight, -this.r + this.offset, this.r);
+        this.ctx.quadraticCurveTo(-this.r / 2 + this.offset, this.r + offsetHeight, 0 + this.offset, this.r);
+        this.ctx.quadraticCurveTo(this.r / 2 + this.offset, this.r - offsetHeight, this.r + this.offset, this.r);
+        this.ctx.quadraticCurveTo(3 * this.r / 2 + this.offset, this.r + offsetHeight, 2 * this.r + this.offset, this.r);
+        this.ctx.lineTo(2 * this.r + this.offset, 2 * this.r);
+        this.ctx.lineTo(-2 * this.r + this.offset, 2 * this.r);
+        this.ctx.lineTo(-2 * this.r + this.offset, this.r);
+        this.ctx.closePath();
+        this.ctx.fill();
+    }
+    draw = () => {
+        this.ctx.clearRect(0, 0, this.r * 2, this.r * 2);
+        this.drawWave();
+        requestAnimationFrame(this.draw);
+    }
+    componentDidMount() {
+        const canvas = this.canvas.current;
+        if (canvas.getContext) {
+            this.ctx = canvas.getContext('2d');
+            this.border = this.borderCanvas.current.getContext('2d');
+            this.clipCircle(this.r - 5);
+            this.drawCircle(this.r);
+            requestAnimationFrame(this.draw);
+        }
+    }
+    render() {
+        return (
+            <div style={{ position: 'relative' }}>
+                <canvas style={{ position: 'absolute' }} ref={this.canvas} width={this.r * 2} height={this.r * 2}></canvas>
+                <canvas style={{ position: 'absolute' }} ref={this.borderCanvas} width={this.r * 2} height={this.r * 2}></canvas>
+            </div>
+        );
+    }
+}
+
+export default WaveProgress;
